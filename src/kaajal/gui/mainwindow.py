f515ccef64306a7fa10cd7a06d825f0df3d58793
Miguel Bernal Marin <miguel.bernal.marin@intel.com>
2025-04-16 11:46:24 -0600

    fix buttons position
    Fixes: d71b60cdfd51 ("gui: put conection type label at top and cosmetics")

--- src/kaajal/gui/mainwindow.py
+++ src/kaajal/gui/mainwindow.py
@@ -86,14 +86,14 @@ class MainWindow(tk.Tk):
             text="Search SSH Key",
             command=lambda: self._open_file(self.ssh_key),
         )
-        btn_ssh_key.grid(column=3, row=4, sticky="we")
+        btn_ssh_key.grid(column=3, row=5, sticky="we")
 
         btn_ssh_config = ttk.Button(
             mainframe,
             text="Search SSH config",
             command=lambda: self._open_file(self.ssh_config),
         )
-        btn_ssh_config.grid(column=3, row=5, sticky="we")
+        btn_ssh_config.grid(column=3, row=6, sticky="we")
 
         ttk.Button(mainframe, text="Connect").grid(column=1, row=9, sticky=tk.W)
 
