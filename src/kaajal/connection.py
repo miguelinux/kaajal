82882613ffe7f54057279e033a7356c237d52c1d
Miguel Bernal Marin <miguel.bernal.marin@intel.com>
2025-04-17 02:54:25 -0600

    conn: start with connect function

--- src/kaajal/connection.py
+++ src/kaajal/connection.py
@@ -24,3 +24,24 @@ class SSHConnection:
         self.config = SSHConfig()
         self.client = paramiko.SSHClient()
         self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())  # nosec B507
+
+    def connect(self, config) -> None:
+        """Connect to the server"""
+
+        if config["connection_type"] == "User":
+            if config["user"] and config["host"] and config["password"]:
+                pass
+            else:
+                logger.error('Missing arguments in "User" connetion type')
+        elif config["connection_type"] == "SSH key":
+            if config["user"] and config["host"] and config["ssh_key"]:
+                pass
+            else:
+                logger.error('Missing arguments in "SSH key" connetion type')
+        elif config["connection_type"] == "SSH host":
+            if config["ssh_config"] and config["ssh_config_host"]:
+                pass
+            else:
+                logger.error('Missing arguments in "SSH host" connetion type')
+        else:
+            logger.error("No connection type setted")
