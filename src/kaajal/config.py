dc14fb63ceb2fb355000bb16c98123295b015701
Miguel Bernal Marin <miguel.bernal.marin@intel.com>
2025-04-20 01:27:07 -0600

    config: add set and save

--- src/kaajal/config.py
+++ src/kaajal/config.py
@@ -54,9 +54,9 @@ def read_config_from(path: str) -> None:
                 if len(item) == two_items:
                     values[item[0].strip().lower()] = item[1].strip().lower()
 
-        for val in config:
-            if val in values:
-                config[val] = values[val]
+        for key in config:
+            if key in values:
+                config[key] = values[key]
 
     else:
         logger.warning("%s: not found", path)
@@ -101,9 +101,6 @@ def load(**kwargs) -> None:
         config_file = os.path.join(ucd, "kaaja.conf")
         if os.path.exists(config_file):
             read_config_from(config_file)
-
-        # logger.warning("Can not create config directory: " + ucd)
-        # appname=__appname__, appauthor=__appauthor__, ensure_exists=True
 
     # 2. virtual environemnt KAAJAL_XXXX
     read_config_env()
@@ -175,3 +172,42 @@ def get_conn_type() -> str:
 
     config["connection_type"] = ret
     return ret
+
+
+def set(cfg_arg: dict) -> None:
+    for key in config:
+        if key in cfg_arg:
+            config[key] = cfg_arg[key]
+
+
+def save() -> None:
+    """Save the config to the default location"""
+
+    ucd = platformdirs.user_config_dir(
+        appname=__appname__, appauthor=__appauthor__, ensure_exists=True
+    )
+    if not os.path.exists(ucd):
+        logger.warning("%s: Can not create directory.", ucd)
+        return
+
+    config_path = os.path.join(ucd, "kaaja.conf")
+
+    str_content = "# Autosaved config\n"
+    str_content += "# vi: set shiftwidth=4 tabstop=8 expandtab: filetype=sh\n"
+    str_content += "# User to configure the platform\n"
+    str_content += "USER=" + config["user"] + "\n\n"
+    str_content += "# User passowrd\n"
+    str_content += "PASSWORD=" + config["password"] + "\n\n"
+    str_content += "# Remote Host IP or DNS name\n"
+    str_content += "HOST=" + config["host"] + "\n\n"
+    str_content += "# User SSH key to connect to the platform\n"
+    str_content += "SSH_KEY=" + config["ssh_key"] + "\n\n"
+    str_content += "# User SSH config file\n"
+    str_content += "SSH_CONFIG=" + config["ssh_config"] + "\n\n"
+    str_content += "# Host from the user SSH config file\n"
+    str_content += "SSH_CONFIG_HOST=" + config["ssh_config_host"] + "\n\n"
+    str_content += "# Type of connection to use (User, SSH key or SSH host)\n"
+    str_content += "CONNECTION_TYPE=" + config["connection_type"] + "\n"
+
+    with open(config_path, mode="w", encoding="utf-8") as conf_file:
+        conf_file.write(str_content)
